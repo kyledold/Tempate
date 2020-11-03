24a0ee7effd025b62438ff7894ac618630858d71
Kyle Dold <kdold02@gmail.com>
2020-11-03 18:55:29 +0000

    Created blank project

---
+++ Template/Template/TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateApp.swift
+//  Template
+//
+//  Created by Kyle Dold on 03/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
